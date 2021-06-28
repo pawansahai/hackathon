38f3f2deae15f9ad779b4eb84cf22a08d0569896
Pawan Priyadarshi <pawan.priyadarshi@allianz.com>
2021-06-28 15:07:32 +0700

    first commit

--- extract.py
+++ extract.py
@@ -7,22 +7,28 @@ import rake
 import operator
 import io
 
-import datetime
+from datetime import datetime
+import datefinder
+import json
 
-inputDate = input("Enter the date in format 'dd/mm/yy' : ")
+sentencesArray = {}
+keywordsArray = {}
+phrasesArray = {}
+datesArray = {}
+sentencesWithDatesArray = {}
 
-day,month,year = inputDate.split('/')
+def convertToJSON(fileName, data):
+  with open(fileName, 'w', encoding="utf8") as outfile:
+       json.dump(data, outfile)
 
-isValidDate = True
-try :
-    datetime.datetime(int(year),int(month),int(day))
-except ValueError :
-    isValidDate = False
+def findDates(keyword):
+   print("find date")
+   matches = datefinder.find_dates(keyword)
+   for match in matches:
+       sentencesWithDatesArray[sentence] = str(match)
+       print(match)
 
-if(isValidDate) :
-    print ("Input date is valid ..")
-else :
-    print ("Input date is not valid..")
+   convertToJSON('output_data/dates.json', sentencesWithDatesArray)
 
 # EXAMPLE ONE - SIMPLE
 stoppath = "data/stoplists/SmartStoplist.txt"
@@ -31,11 +37,11 @@ stoppath = "data/stoplists/SmartStoplist.txt"
 rake_object = rake.Rake(stoppath, 5, 3, 4)
 
 # 2. run on RAKE on a given text
-sample_file = io.open("data/nadhini.txt", 'r',encoding="iso-8859-1")
+sample_file = io.open("data/chat.txt", 'r',encoding="iso-8859-1")
 text = sample_file.read()
 
 keywords = rake_object.run(text)
-
+convertToJSON('output_data/keywords.json', keywords)
 # 3. print results
 print("Keywords:", keywords)
 
@@ -52,13 +58,18 @@ rake_object = rake.Rake(stoppath)
 sentenceList = rake.split_sentences(text)
 
 for sentence in sentenceList:
-    print("Sentence:", sentence)
+    if sentence:
+        print("Sentence:", sentence)
+        findDates(sentence)
+
+convertToJSON('output_data/sentences.json', sentenceList)
 
 # generate candidate keywords
 stopwords = rake.load_stop_words(stoppath)
 stopwordpattern = rake.build_stop_word_regex(stoppath)
 phraseList = rake.generate_candidate_keywords(sentenceList, stopwordpattern, stopwords)
 print("Phrases:", phraseList)
+convertToJSON('output_data/phrases.json', phraseList)
 
 # calculate individual word scores
 wordscores = rake.calculate_word_scores(phraseList)
@@ -67,13 +78,20 @@ wordscores = rake.calculate_word_scores(phraseList)
 keywordcandidates = rake.generate_candidate_keyword_scores(phraseList, wordscores)
 for candidate in keywordcandidates.keys():
     print("Candidate: ", candidate, ", score: ", keywordcandidates.get(candidate))
+convertToJSON('output_data/candidates.json', keywordcandidates)
 
 # sort candidates by score to determine top-scoring keywords
 sortedKeywords = sorted(six.iteritems(keywordcandidates), key=operator.itemgetter(1), reverse=True)
 totalKeywords = len(sortedKeywords)
 
 # for example, you could just take the top third as the final keywords
-for keyword in sortedKeywords[0:int(totalKeywords / 3)]:
+for keyword in sortedKeywords[0:int(totalKeywords)]:
     print("Keyword: ", keyword[0], ", score: ", keyword[1])
 
 print(rake_object.run(text))
+
+for x in sentencesWithDatesArray:
+  print(x)
+  print(sentencesWithDatesArray[x])
+
+
